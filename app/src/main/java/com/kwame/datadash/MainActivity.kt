3b4487145c9e60070aed83f67b4b203f427648a7
tunucaleb969-crypto <tunucaleb969@gmail.com>
2026-09-16 09:55:33 +0000

    Fix crash: import androidx.appcompat.widget.SearchView (matches menu XML) instead of the framework SearchView

--- app/src/main/java/com/kwame/datadash/MainActivity.kt
+++ app/src/main/java/com/kwame/datadash/MainActivity.kt
@@ -5,10 +5,10 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.SearchView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
